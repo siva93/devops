f1f61c7956e4110ea16b9920cfcd76c77ee8d691
siva93 <b.siva93@gmail.com>
2021-12-30 17:51:02 +0530

    Update main.tf
    added azure rm IaC

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
-terraform{
-  
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "terraform"
+  location = "westus2"
 }
